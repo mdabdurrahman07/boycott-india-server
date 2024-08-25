185c872155fe26118c4b1b7d61ac79c23ec0c4b3
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-08-25 17:04:15 +0600

    all products delete and update routes implemented

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
-const port = process.env.PORT
+const port = process.env.PORT;
 const cors = require("cors");
-const allproduct = require('./src/model/allproduct');
-
+const allproduct = require("./src/model/allproduct");
 
 // middlewares
 app.use(cors({ origin: "http://localhost:5173" }));
@@ -13,37 +12,65 @@ app.use(express.json());
 
 // mongoose connection
 mongoose
-  .connect(
-    `${process.env.MongoDBConnection}`,
-    { dbName: "BoycottIndianProduct"}
-  )
+  .connect(`${process.env.MongoDBConnection}`, {
+    dbName: "BoycottIndianProduct",
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("mongodb connection error", err));
 
-app.get('/', (req, res) => {
-  res.send('Boycott Indian Products server')
-})
+app.get("/", (req, res) => {
+  res.send("Boycott Indian Products server");
+});
 
 // all post routes
 
-app.post('/api/bcip/v1/allproducts/post', async (req , res) => {
+app.post("/api/bcip/v1/allproducts/post", async (req, res) => {
   try {
-    const data = req.body
-    const newProduct = allproduct(data)
-  const result = await newProduct.save()
-  res.send(result)
+    const data = req.body;
+    const newProduct = allproduct(data);
+    const result = await newProduct.save();
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// all get routes
+
+app.get("/api/bcip/v1/allproducts/get", async (req, res) => {
+  const result = await allproduct.find();
+  res.send(result);
+});
+
+// all updates routes
+
+app.put("/api/bcip/v1/allproducts/update/:id", async (req, res) => {
+  try {
+    const updatedData = req.body;
+    const id = req.params.id;
+    const result = await allproduct.updateOne(
+      { _id: id },
+      { $set: updatedData },
+      { upsert: true }
+    );
+    res.send(result)
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// all delete routes
+
+app.delete('/api/bcip/v1/allproducts/delete/:id' , async (req, res) => {
+  try {
+    const id = req.params.id
+    const result = await allproduct.deleteOne({_id: id})
+    res.send(result)
   } catch (error) {
     res.status(500).send(error.message);
   }
 })
 
-// alll get routes
-
-app.get('/api/bcip/v1/allproducts/get', async (req , res) => {
-    const result = await allproduct.find()
-    res.send(result)
-})
-
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
